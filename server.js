15a61fe5773949fa5bb41b335e26e4ee8fb1af4f
Carlos Villalta <CarlosFindley@protonmail.com>
2019-01-06 20:54:15 -0800

    update

--- server.js
+++ server.js
@@ -34,9 +34,8 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-var routes = require(".controllers/burgers_controllers.js");
-
-app.use(routes);
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
