50ad63459f656fa039af22b09c4d97796c89338c
Carlos Villalta <CarlosFindley@protonmail.com>
2019-01-06 22:23:17 -0800

    update

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kobe Beef Burger", FALSE);
