3d21c12dbc0ba5e7685b52cf3f07d1a2fa285c9f
Carlos Villalta <CarlosFindley@protonmail.com>
2019-01-09 13:56:46 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,13 @@
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-} else {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-	var connection = mysql.createConnection({
+var connection = mysql.createConnection({
 		host     : "localhost",
   		port     : 3306,
   		user     : "root",
   		password : "password",
-  		database: "burgers_db"
-	});
-};
-	
+  		database: 'burgers_db'
+});
+
 
 connection.connect(function(err) {
 	if (err) {
